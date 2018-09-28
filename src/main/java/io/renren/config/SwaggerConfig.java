edc61fb5afa76727f6a34d08f49b20fd10cfc281
kuzi <kuzi199398@163.com>
2018-09-28 17:02:45 +0800

    提交

--- src/main/java/io/renren/config/SwaggerConfig.java
+++ src/main/java/io/renren/config/SwaggerConfig.java
@@ -27,6 +27,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
+        registry.addResourceHandler("/yunxiao/**").addResourceLocations("classpath:/static/yunxiao/");
     }
 
 
